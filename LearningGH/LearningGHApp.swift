f8768d8ddee3a3c5feb8ac8c5e8dd6e3bc9c1985
Andy <and.hammond12@gmail.com>
2021-09-22 09:24:10 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Andy Hammond on 2021-09-22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
